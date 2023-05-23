b701371e043c3df009ecb492ee1f29586902ef22
Badhon sarker <badhonsarker700@gmail.com>
2023-05-23 23:10:41 +0600

    tab category view details api create

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express")
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors")
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 //middleware
-app.use(cors())
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -14,8 +20,8 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.zwfejkx.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zwfejkx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,26 +36,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
-    
-const teddyCollection = client.db("teddyDb").collection("teddys")
-const bookToyCollection = client.db("teddyDb").collection("bookToys")
-const toyGalleryCollection = client.db("teddyDb").collection("toyGallery")
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+    client.connect((error)=>{
+      if(error){
+        console.log(error)
+        return;
+      }
+    });
 
 
-
+    const teddyCollection = client.db("teddyDb").collection("teddys")
+    const bookToyCollection = client.db("teddyDb").collection("bookToys")
+    const toyGalleryCollection = client.db("teddyDb").collection("toyGallery")
     app.get("/toyGallery", async (req, res) => {
-      const result = await toyGalleryCollection.find().toArray()
+      const cursor = toyGalleryCollection.find()
+      const result = await cursor.toArray()
       res.send(result)
     })
 
@@ -58,7 +58,14 @@ run().catch(console.dir);
       const result = await teddyCollection.find().toArray()
       res.send(result)
     })
-
+    app.get('/teddys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+        const result = await teddyCollection.findOne(query);
+        res.send(result);
+        res.status(400).send('Error Retrieving Toy Details');
+   
+    });
 
     app.get('/bookToys', async (req, res) => {
       let query = {}
@@ -116,6 +123,15 @@ run().catch(console.dir);
       const result = await bookToyCollection.deleteOne(query);
       res.send(result);
     })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
